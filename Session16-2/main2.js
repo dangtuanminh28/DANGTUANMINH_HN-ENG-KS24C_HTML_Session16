14164f10d7a5bcd5427c37663c7864262f9d39fd
đặng mạnh trường <manhtruong2497@gmail.com>
2025-02-26 23:02:22 +0700

    s16

--- Session16-2/main2.js
+++ Session16-2/main2.js
@@ -1,4 +1,9 @@
-let number = 10;
-let price = 20.5;
-let status = number + price;
-let city = "Hà Nội";
+var number = 10;
+var price = 20.5;
+var status = true;
+var city = "Hà Nội";
+
+document.write(number + "<br>");
+document.write(price + "<br>");
+document.write(status + "<br>");
+document.write(city);
